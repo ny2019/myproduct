9ba8134bb5ef11944688018e650174a68a89a89a
ny2019 <51043403+ny2019@users.noreply.github.com>
2019-05-26 12:01:41 +0430

    Add files via upload
    it is my calculator.

---
+++ MyCalculate/MyCalculate/Invers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculate
+{
+   public class Invers
+    {
+       string num;
+       int x=0;
+       int len;
+       int n;
+       public string Num
+       {
+           get { return num; }
+           set { num = value; }
+       }
+       public int CalInvers()
+       {
+           len = num.Length;
+           n = int.Parse(num);
+
+           for (int i = 1; i <= len ;i++ )
+           {
+               x += (int)((n % 10) * Math.Pow(10, (len - i)));
+               n = n / 10;
+               return x;
+           }
+           return x;
+
+
+       }
+
+    }
+}
---
+++ MyCalculate/MyCalculate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invers inobj = new Invers();
+            Console.Write(" Enter your number: ");
+            string num = Console.ReadLine();
+            int m = inobj.CalInvers();
+            Console.WriteLine("your result is:{0} ",m.ToString());
+            Console.ReadKey();
+
+            
+        }
+    }
+}
